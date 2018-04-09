69ac2ba0077d10e5774f4928f7304f4f33435d9a
ILYA <yusupov.ilya.84@gmail.com>
2018-04-09 13:09:53 +0300

    девятый урок

--- project_2/js/script.js
+++ project_2/js/script.js
@@ -79,4 +79,30 @@ window.addEventListener('DOMContentLoaded', function(){
 	};
 
 	setClock('timer', deadline);
+// modal
+
+	let more = document.querySelector('.more'),
+			overlay = document.querySelector('.overlay'),
+			close = document.querySelector('.popup-close'),
+			description_btn = document.querySelectorAll('.description-btn');
+	more.addEventListener('click', function() {		
+		this.classList.add('more-splash');
+		overlay.style.display = "block";
+		document.body.style.overflow = "hidden";
+	});		
+
+	for (let i = 0; i < description_btn.length; i++) {
+			description_btn[i].addEventListener( 'click', function() {
+				this.classList.add('more-splash');
+				overlay.style.display = "block";
+				document.body.style.overflow = "hidden";
+			});
+		};
+
+	close.addEventListener('click', function() {
+		overlay.style.display = "none";
+		more.classList.remove('more-splash');
+		document.body.style.overflow = "";
+	});
+
 });
