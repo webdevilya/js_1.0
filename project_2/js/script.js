58e22967669028434a1963e230dac36cdbd58aad
ILYA <yusupov.ilya.84@gmail.com>
2018-04-12 14:20:48 +0300

    13 урок

--- project_2/js/script.js
+++ project_2/js/script.js
@@ -149,5 +149,103 @@ window.addEventListener('DOMContentLoaded', function(){
 				}
 			});
 
+	//slider
+	
+	let sliderIndex = 1,
+			slides = document.getElementsByClassName('slider-item'),
+			prev = document.querySelector('.prev'),
+			next = document.querySelector('.next'),
+			dotsWrap = document.querySelector('.slider-dots'),
+			dots = document.getElementsByClassName('dot');
+
+			showSlides(sliderIndex);
+
+			function showSlides(n) {
+
+				if (n > slides.length) {
+					sliderIndex = 1;
+				};
+				if (n < 1) {
+					sliderIndex = slides.length;
+				};
+
+				for (let i = 0; i < slides.length; i++) {
+					slides[i].style.display = 'none';
+				};
+
+				for (let i = 0; i < dots.length; i++) {
+					dots[i].classList.remove('dot-active');
+				};
+
+				slides[sliderIndex - 1].style.display = 'block';
+				dots[sliderIndex - 1].classList.add('dot-active');
+			}	
+
+			function plusSlides (n) {
+				showSlides(sliderIndex += n)
+			}
+
+			function currentSlide(n) {
+				showSlides(sliderIndex = n)
+			}
+
+			prev.addEventListener('click', function () {
+				plusSlides(-1);
+			});	
+
+			next.addEventListener('click', function () {
+				plusSlides(1);
+			});	
+
+			dotsWrap.addEventListener('click', function(event) {
+				for (let i =0; i < dots.length + 1; i++) {
+					if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+						currentSlide(i)
+					}
+				}
+			});
+
+	//calc
+
+	let persons = document.getElementsByClassName('counter-block-input')[0],
+			restDays = document.getElementsByClassName('counter-block-input')[1],
+			place = document.getElementById('select'),
+			totalValue = document.getElementById('total'),
+			personsSum = 0,
+			daysSum = 0,
+			total = 0;
+
+			totalValue.innerHTML = 0;
+
+			persons.addEventListener('change', function() {
+				personsSum = +this.value;
+				total =(daysSum + personsSum) * 4000;
+				if (restDays.value == '') {
+					totalValue.innerHTML = 0; 
+				} else {
+					totalValue.innerHTML = total;
+				}
+			});
+			restDays.addEventListener('change', function() {
+				personsSum = +this.value;
+				total =(daysSum + personsSum) * 4000;
+				if (restDays.value == '') {
+					totalValue.innerHTML = 0; 
+				} else {
+					totalValue.innerHTML = total;
+				}
+			});
+
+			place.addEventListener('change', function() {
+				if (restDays.value == '' || persons.value == '') {
+					totalValue.innerHTML = 0;
+				} else {
+					let a = total;
+					totalValue.innerHTML = a * this.options[this. selectedIndex].value;
+				}
+			})
+	
+
+
 });	
 	
