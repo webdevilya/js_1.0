c95a836c17d9dcdb59344a989d386f2363ff2875
ILYA <yusupov.ilya.84@gmail.com>
2018-04-12 09:41:16 +0300

    11 урок

--- project_2/js/script.js
+++ project_2/js/script.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', function(){
 			tabContent[i].classList.remove('show');
 			tabContent[i].classList.add('hide');
 		}  
-	};	
+	}
 
 	hideTabContent(1);
 
@@ -19,7 +19,7 @@ window.addEventListener('DOMContentLoaded', function(){
 			tabContent[b].classList.remove('hide');
 			tabContent[b].classList.add('show');
 		}
-	};
+	}
 
 	info.addEventListener('click', function(event){
 		let target = event.target;
@@ -49,7 +49,7 @@ window.addEventListener('DOMContentLoaded', function(){
 					'minutes': minutes,
 					'seconds': seconds
 				};
-	};
+	}
 
 	function setClock (id, endtime) {
 
@@ -71,12 +71,12 @@ window.addEventListener('DOMContentLoaded', function(){
 						seconds.innerHTML = '00';
 
 					}
-				};
+				}
 
 				updateClock();
 				
 
-	};
+	}
 
 	setClock('timer', deadline);
 // modal
@@ -104,5 +104,50 @@ window.addEventListener('DOMContentLoaded', function(){
 		more.classList.remove('more-splash');
 		document.body.style.overflow = "";
 	});
-	
+
+	//Form
+
+	let message = new Object();
+	message.loading = "Загрузка....";
+	message.success = "Спасибо! Скоро мы с вами свяжемся.";
+	message.failure = "Что-то пошло не так.....";
+
+	let form = document.getElementsByClassName('main-form')[0],
+			input = form.getElementsByTagName('input');
+			statusMessage = document.createElement('div');
+			statusMessage.classList.add('status');
+
+			form.addEventListener('submit', function(event) {
+				event.preventDefault();
+				form.appendChild(statusMessage);
+
+				//AJAX
+
+				let request = new XMLHttpRequest();
+				request.open("POST", 'server.php');
+
+				request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+				let formData = new FormData(form);
+
+				request.send(formData);
+
+				request.onreadystatechange = function() {
+					if (request.readyState < 4) {
+						statusMessage.innerHTML = message.loading;
+					} else if (request.readyState === 4) {
+						if (request.status == 200 && request.status < 300) {
+							statusMessage.innerHTML = message.success;
+							//добавляем контент на страницу
+						} else {
+							statusMessage.innerHTML = message.failure;
+						}
+					}
+				}
+				for ( let i =0; i < input.length; i++) {
+					input[i].value = '';
+				}
+			});
+
+});	
 	
